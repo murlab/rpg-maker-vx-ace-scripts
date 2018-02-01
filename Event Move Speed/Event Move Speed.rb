fe7a2387218b97ed27cc98c4e9827157ee5ff7c5
Mur <34001763+murlab@users.noreply.github.com>
2018-02-01 14:55:14 +0300

    Доработка «Event Move Speed»

--- Event Move Speed/Event Move Speed.rb
+++ Event Move Speed/Event Move Speed.rb
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Event Move Speed v1.0
+# Event Move Speed v1.1
 # by MUR (https://github.com/murlab)
 # BSD 3-Clause License
 # Free for use with both free and commercial RPG Maker games.
@@ -10,20 +10,29 @@ class Game_CharacterBase
   def initialize
     mur_game_charbase_init
     @event_speed = 0
+    @next_frame = 1
   end
-  
-  def get_event_speed
-    return @event_speed
-  end
-  
+ 
   def set_event_speed(speed)
     @event_speed = speed
+  end
+
+  def set_next_frame(next_frame)
+    @next_frame = next_frame
   end
   
   def real_move_speed  
     @move_speed + @event_speed
   end
 
+  def update_anime_count
+    if moving? && @walk_anime
+      @anime_count += @next_frame + 0.5
+    elsif @step_anime || @pattern != @original_pattern
+      @anime_count += @next_frame
+    end
+  end
+  
 end
 
 class Game_Map
@@ -36,9 +45,15 @@ class Game_Map
   def set_events_speed
     @events.values.each { |e|
       next if !e.list
-      if e.list[0].code == 108 && e.list[0].parameters[0] =~ /<speed:(.*)>/
-        e.set_event_speed($1.to_f)
-#        puts e.get_event_speed
+      if e.list[0].code == 108
+        if e.list[0].parameters[0] =~ /<speed:(.*)>/
+          e.set_event_speed($1.to_f)
+          #puts e.get_event_speed
+        end
+        if e.list[0].parameters[0] =~ /<nextframe:(.*)>/
+          e.set_next_frame($1.to_f)
+          #puts e.@next_frame
+        end
       end
     }
   end
